8614109e5246ffe424cf963aa3d1cb5627af11a8
Nabzah1800 <nabil.zahmidi@student.odisee.be>
2025-12-09 14:29:33 +0100

    Opdrachten: Git CLI + Python venv + Unit Testing + Docker

---
+++ unit_testing/test_calc.py
@@ -0,0 +1,14 @@
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.add(2, 2), 5)
+        self.assertEqual(calc.add(-1, 1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(2, 3), 6)
+        self.assertEqual(calc.multiply(-1, 3), -3)
+
+if __name__ == "__main__":
+    unittest.main()
